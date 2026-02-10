7a6478ff542290b14022d5507103bf66c13fc386
SaidBC <razzouksaid139@gmail.com>
2026-02-10 15:44:31 +0100

    feat: Setup monetag verify meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,6 @@ import { RecentRewardsSection } from "@/ui/RecentRewardsSection";
 import FaqSection from "@/ui/FaqSection";
 import PlatformsListSection from "@/ui/PlatformsListSection";
 
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "NewFreeRewards – Free Rewards, Bonuses & Promotions",
-  description:
-    "Discover free rewards, bonuses, promo codes, and giveaways from games and online platforms. No hacks. Updated daily.",
-  openGraph: {
-    title: "NewFreeRewards – Free Rewards & Bonuses",
-    description:
-      "Find free rewards from games and online platforms. Emotes, bonuses, credits, and more.",
-    url: "/",
-    type: "website",
-  },
-};
-
 export default function Page() {
   return (
     <main className="px-8 ">
