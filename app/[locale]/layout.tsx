53677ec81cc53884eb5580e46dfed38fd4be8783
SaidBC <razzouksaid139@gmail.com>
2026-04-04 01:13:34 +0100

    refactor: remove MonetageVignette component and production environment check from layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -9,8 +9,6 @@ import {
   locales,
   type Locale,
 } from "@/lib/i18n";
-import MonetageVignette from "@/components/MonetageVignette";
-import clientEnv from "@/utils/clientEnv";
 
 export const dynamic = "force-static";
 export const dynamicParams = false;
@@ -53,7 +51,6 @@ export default async function LocaleLayout({
       <Header locale={locale} />
       {children}
       <Footer locale={locale} />
-      {clientEnv.NEXT_PUBLIC_NODE_ENV === "production" && <MonetageVignette />}
     </div>
   );
 }
